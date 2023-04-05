cea6cf7a521038dd864b2642c9c119864d388cdd
Juan Lopez <jlopezj@proveedor.intercorp.com.pe>
2023-04-04 23:19:22 -0500

    Request and transform & management errors

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 
 import { CreateProductDTO, UpdateProductDTO, Product } from './../models/product.model';
-import { retry } from 'rxjs';
+import { retry, catchError, throwError, map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,12 +22,32 @@ export class ProductsService {
     }
     return this.http.get<Product[]>(this.apiUrl, { params })
       .pipe(
-        retry(3)
-      );
+        retry(3),
+        map(products => products.map(item => {
+          return {
+            ...item,
+            taxes: .19 * item.price
+          }
+        })
+      ));
   }
 
   getProduct(id: string) {
     return this.http.get<Product>(`${this.apiUrl}/${id}`)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === HttpStatusCode.Conflict) {
+          return throwError('Algo está fallando en el server');
+        }
+        if (error.status === HttpStatusCode.NotFound) {
+          return throwError('El producto no existe')
+        }
+        if (error.status === HttpStatusCode.Unauthorized) {
+          return throwError('No estás permitido')
+        }
+        return throwError('Ups algo salio mal');
+      })
+    )
   }
 
   getProductsByPage(limit: number, offset: number) {
