ef6fe64488843cae4e546daef7caaa04f2403623
Juan Lopez <jlopezj@proveedor.intercorp.com.pe>
2023-04-20 14:30:31 -0500

    Login and Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { Product } from './models/product.model';
+// import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,10 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService
+  ) { }
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -18,5 +24,23 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.userService.create({
+      name: 'Juan',
+      email: 'juanlopez.developer@gmail.com',
+      password: '1234'
+    })
+      .subscribe(rta => {
+        console.log(rta);
+      })
+  }
+
+  login() {
+    this.authService.login('juanlopez.developer@gmail.com', '1234')
+      .subscribe(rta => {
+        console.log(rta.access_token);
+      })
   }
 }
