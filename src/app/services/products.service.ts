083f51ec479f90d87cbfdd596472bc7d02cb0e58
Juan Lopez <jlopezj@proveedor.intercorp.com.pe>
2023-04-24 17:36:35 -0500

    Create a interceptor context (Run the interceptor only when you need it)

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angu
 
 import { CreateProductDTO, UpdateProductDTO, Product } from './../models/product.model';
 import { retry, catchError, throwError, map, zip } from 'rxjs';
+import { checkTime } from './../interceptors/time.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,7 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params })
+    return this.http.get<Product[]>(this.apiUrl, { params, context: checkTime()})
       .pipe(
         retry(3),
         map(products => products.map(item => {
